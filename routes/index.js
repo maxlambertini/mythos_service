5d5ab766a9c65b3b862e275a5b5473437834698c
max lambertini <maxl@localhost.localdomain>
2013-08-29 07:07:08 +0200

    Made the design a little more responsive

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var crit = new mythos.Cthulhu();
 exports.index = function(req, res){
     var adj = crit.getAdjective(true);
   res.render('index', { 
-      title: 'Mythos as a Service: The Random, Undeterministic Source  Of Lovecraftian Mythos',
+      title: 'The Random, Undeterministic Source  Of Lovecraftian Mythos',
       blurb: "Mythos as a Service",
       subblurb: 'The ' + adj + ' Source of Lovecraftian Mythos',
       mythos: crit
